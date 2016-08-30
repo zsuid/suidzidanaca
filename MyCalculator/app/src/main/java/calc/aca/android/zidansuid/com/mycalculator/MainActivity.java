19f2d1f4b25880954802fa4f81d1013b7f2ffbd6
zidan Suid <zidansuid@gmail.com>
2016-08-30 13:19:04 -0500

    initial commit initial project setup

--- MyCalculator/app/src/main/java/calc/aca/android/zidansuid/com/mycalculator/MainActivity.java
+++ MyCalculator/app/src/main/java/calc/aca/android/zidansuid/com/mycalculator/MainActivity.java
@@ -1,5 +1,6 @@
 package calc.aca.android.zidansuid.com.mycalculator;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -8,45 +9,54 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private com.google.android.gms.common.api.GoogleApiClient client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new com.google.android.gms.common.api.GoogleApiClient.Builder(this).addApi(com.google.android.gms.appindexing.AppIndex.API).build();
     }
-    public void onButtonClick(View v)
-    {
-        EditText a1 = (EditText)findViewById(R.id.TFnum1);
-        EditText a2 = (EditText)findViewById(R.id.TFnum2);
 
-        TextView tv = (TextView)findViewById(R.id.Lresult);
+    public void onButtonClick(View v) {
+        EditText a1 = (EditText) findViewById(R.id.TFnum1);
+        EditText a2 = (EditText) findViewById(R.id.TFnum2);
+
+        TextView tv = (TextView) findViewById(R.id.Lresult);
 
         boolean flag = false;
 
-        double num1,num2,ans;
+        double num1, num2, ans;
         num1 = Double.parseDouble(a1.getText().toString());
         num2 = Double.parseDouble(a2.getText().toString());
         ans = 0;
 
-        if(v.getId() == R.id.Badd)
+        if (v.getId() == R.id.Badd)
             ans = num1 + num2;
 
-        if(v.getId() == R.id.Bsub)
+        if (v.getId() == R.id.Bsub)
             ans = num1 - num2;
 
 
-        if(v.getId() == R.id.Bmult)
-        ans = num1 * num2;
+        if (v.getId() == R.id.Bmult)
+            ans = num1 * num2;
 
-        if(v.getId() == R.id.Bdiv)
+        if (v.getId() == R.id.Bdiv)
             if (num2 == 0)
-               flag = true;
+                flag = true;
             else
-        ans = num1 / num2;
+                ans = num1 / num2;
 
-        tv.setText(ans+"");
-
-
+        tv.setText(ans + "");
 
 
     }
+
+
 }
