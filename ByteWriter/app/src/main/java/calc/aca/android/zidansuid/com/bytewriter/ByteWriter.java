49bd76f640812e85a2601988e93d056dc4709f23
zidan Suid <zidansuid@gmail.com>
2016-09-07 15:13:13 -0500

    commit codekeeper2

--- ByteWriter/app/src/main/java/calc/aca/android/zidansuid/com/bytewriter/ByteWriter.java
+++ ByteWriter/app/src/main/java/calc/aca/android/zidansuid/com/bytewriter/ByteWriter.java
@@ -1,31 +1,45 @@
 package calc.aca.android.zidansuid.com.bytewriter;
 
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class ByteWriter extends AppCompatActivity {
 
-   @Override
-   protected void onCreate(Bundle savedInstanceState) {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-      setContentView(R.layout.activity_byte_writer);
-   }
+        setContentView(R.layout.activity_byte_writer);
+
+        int[] data = {71, 73, 70, 56, 57, 97, 13, 0, 12, 0, 145,
+                0, 0, 255, 255, 255, 255, 255, 0, 0, 0, 0, 0, 0, 0, 44,
+                0, 0, 0, 0, 13, 0, 12, 0, 0, 2, 38, 132, 45, 121, 11,
+                25, 175, 150, 120, 20, 162, 132, 51, 110, 106, 239, 22,
+                8, 160, 56, 137, 96, 72, 77, 33, 130, 86, 37, 219, 182,
+                230, 137, 89, 82, 181, 50, 220, 103, 20, 0, 59};
 
 
-        public static void main(String[] arguments) {
-                int[] data = { 137, 89, 82, 181, 50, 220, 103, 20, 0, 59 };
-                try {
-                        FileOutputStream file = new FileOutputStream("pic.dat");
-                        for (int i = 0; i < data.length; i++)
-                                file.write(data[i]);
-                        file.close();
-                } catch (IOException e) {
-                        System.out.println("Error - " + e.toString());
-                }
+         File outputFile = new File("/sdcard/pic.gif");
+       // File show = new (Environment.getExternalStorageDirectory().getpath("pic.gif"));
+
+        FileOutputStream fos;
+
+        try {
+           // fos = new FileOutputStream(show);
+            fos = new FileOutputStream(outputFile);
+
+            for (int i = 0; i < data.length; i++) {
+                fos.write(data[i]);
+            }
+            fos.close();
+        } catch (IOException e) {
+            System.out.println("Error - " + e.getMessage());
         }
 
-}
+    }
 }
