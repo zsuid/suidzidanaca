9a985be3a0652be3cce6ca746dcbfd2852044035
zidan Suid <zidansuid@gmail.com>
2016-09-10 09:08:06 -0500

    commit All Home Work untill 9/6/2016

--- ByteWriter/app/src/main/java/calc/aca/android/zidansuid/com/bytewriter/ByteWriter.java
+++ ByteWriter/app/src/main/java/calc/aca/android/zidansuid/com/bytewriter/ByteWriter.java
@@ -1,36 +1,43 @@
 package calc.aca.android.zidansuid.com.bytewriter;
 
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class ByteWriter extends AppCompatActivity {
 
+    ImageView mImageView;
+    TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_byte_writer);
 
-        int[] data = {71, 73, 70, 56, 57, 97, 13, 0, 12, 0, 145,
+        mImageView = (ImageView) findViewById(R.id.imageView);
+        mTextView = (TextView) findViewById(R.id.textView);
+        mTextView.setText("");
+
+        int[] data = { 71, 73, 70, 56, 57, 97, 13, 0, 12, 0, 145,
                 0, 0, 255, 255, 255, 255, 255, 0, 0, 0, 0, 0, 0, 0, 44,
                 0, 0, 0, 0, 13, 0, 12, 0, 0, 2, 38, 132, 45, 121, 11,
                 25, 175, 150, 120, 20, 162, 132, 51, 110, 106, 239, 22,
                 8, 160, 56, 137, 96, 72, 77, 33, 130, 86, 37, 219, 182,
-                230, 137, 89, 82, 181, 50, 220, 103, 20, 0, 59};
+                230, 137, 89, 82, 181, 50, 220, 103, 20, 0, 59 };
 
-
-         File outputFile = new File("/sdcard/pic.gif");
-       // File show = new (Environment.getExternalStorageDirectory().getpath("pic.gif"));
-
+        File outputFile = new File("/sdcard/pic.gif");
         FileOutputStream fos;
 
         try {
-           // fos = new FileOutputStream(show);
             fos = new FileOutputStream(outputFile);
 
             for (int i = 0; i < data.length; i++) {
@@ -41,5 +48,16 @@ public class ByteWriter extends AppCompatActivity {
             System.out.println("Error - " + e.getMessage());
         }
 
+
+        File inputFile = new File("/sdcard/pic.gif");
+        FileInputStream fis;
+
+        try {
+            fis = new FileInputStream(inputFile);
+            Bitmap bmp = BitmapFactory.decodeStream(fis);
+            mImageView.setImageBitmap(bmp);
+        } catch (IOException e) {
+            System.out.println("Error - " + e.getMessage());
+        }
     }
 }
--- Holidayschedule/app/src/main/java/calc/aca/android/zidansuid/com/holidayschedule/MainActivity.java
+++ Holidayschedule/app/src/main/java/calc/aca/android/zidansuid/com/holidayschedule/MainActivity.java
@@ -2,70 +2,82 @@ package calc.aca.android.zidansuid.com.holidayschedule;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.BitSet;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    EditText mdayno;
+    TextView mvresult;
+    Button mvbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        HolidaySchedule cal = new HolidaySchedule();
-        String day = "2";
+        mdayno = (EditText) findViewById(R.id.DayNo);
+        mvresult = (TextView) findViewById(R.id.VResult);
+        mvbutton = (Button) findViewById(R.id.Vbutton);
 
-        if (!day.equals("")) {
-            try {
-                int whichDay = Integer.parseInt(day);
-                if (cal.isHoliday(whichDay)) {
-                    System.out.println("Day number " + whichDay +
-                            " is a holiday.");
-                } else {
-                    System.out.println("Day number " + whichDay +
-                            " is not a holiday.");
+        mvbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                HolidaySchedule cal = new HolidaySchedule();
+
+
+                String day = mdayno.getText().toString();
+                if (!day.equals("")) {
+                    try {
+                        int whichDay = Integer.parseInt(day);
+                        if (cal.isHoliday(whichDay)) {
+                            mvresult.append("Day Number " + whichDay + "is a holiday" + "\n");
+
+                        } else {
+                            mvresult.append("Day Number " + whichDay + "is  not a holiday" + "\n");
+
+                        }
+                    } catch (NumberFormatException nfe) {
+                        System.out.println("Error: " + nfe.getMessage());
+                    }
                 }
-            } catch (NumberFormatException nfe) {
-                System.out.println("Error: " + nfe.getMessage());
+
             }
-        }
-    }
 
-    public class HolidaySchedule {
-        BitSet sked;
+            class HolidaySchedule {
+                BitSet sked;
 
-        public HolidaySchedule() {
-            sked = new BitSet(365);
-            int[] holiday = { 1, 15, 50, 148, 185, 246,
-                    281, 316, 326, 359 };
 
-            for (int i = 0; i < holiday.length; i++) {
-                addHoliday(holiday[i]);
+
+                public HolidaySchedule() {
+                    sked = new BitSet(365);
+
+                    int[] holiday = {1, 15, 50, 148, 185, 246,
+                            281, 316, 326, 359};
+
+                    for (int i = 0; i < holiday.length; i++) {
+                        addHoliday(holiday[i]);
+
+
+                    }
+                }
+
+                public void addHoliday(int dayToAdd) {
+                    sked.set(dayToAdd);
+                }
+
+                public boolean isHoliday(int dayToCheck) {
+                    return sked.get(dayToCheck);
+                }
             }
-        }
 
-        public void addHoliday(int dayToAdd) {
-            sked.set(dayToAdd);
-        }
-
-        public boolean isHoliday(int dayToCheck) {
-            return sked.get(dayToCheck);
-        }
+        });
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
